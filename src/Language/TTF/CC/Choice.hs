cb502278162d520b189d5355231563aa01ce2488
Eric Walkingshaw <eric@walkingshaw.net>
2013-10-07 14:17:49 +0200

    Add binary, dimensioned, and formula choices with pretty printing.

--- src/Language/TTF/CC/Choice.hs
+++ src/Language/TTF/CC/Choice.hs
@@ -1,12 +1,97 @@
 {-# LANGUAGE
       FlexibleInstances,
-      PatternGuards,
-      TypeSynonymInstances #-}
+      MultiParamTypeClasses,
+      OverloadedStrings,
+      PatternGuards
+  #-}
 
 -- | Variational values and a simple variation metalanguage.
 module Language.TTF.CC.Choice where
 
-import Data.List (intersperse)
+import Data.List (intercalate,intersperse)
+import Data.Monoid ((<>),mconcat)
+import GHC.Exts (fromString)
+
+import Language.TTF.Literal
+import Language.TTF.Pretty
+
+
+-- * Variational Values
+
+-- | A tag represents a single configuration option.
+type Tag = String
+
+-- | A configuration is identified by a list of tags (the selected
+--   configuration options).
+type Config = [Tag]
+
+
+-- * Choices
+
+-- | A choice introduces a variation point as a choice between alternatives.
+--   Ultimately, when a configuration is selected, each choice will be
+--   replaced by one of its alternatives, but there are many different ways
+--   of relating the selected configuration to alternatives.
+
+
+-- ** Binary choices
+
+-- | A binary choice is associated with a single tag.
+--   The first alternative is used if the tag is selected,
+--   the second alternative is used if the tag is not selected.
+class ChcT r where
+  chcT :: Tag -> r a -> r a -> r a
+
+instance ChcT (Pretty c) where
+  chcT t a b = fromString t <> "‹" <> a <> "," <> b <> "›"
+
+
+-- ** Dimensioned choices
+
+-- | A dimension defines a list of mutually exclusive tags.
+type Dim = String
+
+-- | A dimensioned choice is associated with a dimension.
+--   Each alternative corresponds to the tag at the same position
+--   in its binding dimension declaration.
+class ChcD r where
+  dim  :: Dim -> [Tag] -> r a -> r a
+  chcD :: Dim -> [r a] -> r a
+
+instance ChcD (Pretty c) where
+  dim  d ts e = fromString d <> "‹" <> fromString (intercalate "," ts) <> "› " <> e
+  chcD d as   = fromString d <> "‹" <> mconcat (intersperse "," as) <> "›"
+
+
+-- ** Formula choices
+
+-- | Boolean tag expressions.
+data Formula = FTag Tag
+             | FNot Formula
+             | FAnd Formula Formula
+             | FOr  Formula Formula
+  deriving Eq
+
+instance Show Formula where
+  show f = case f of
+      FAnd f g -> paren f ++ "∧" ++ paren g
+      FOr  f g -> paren f ++ "∨" ++ paren g
+      f        -> paren f
+    where
+      paren (FTag t) = t
+      paren (FNot f) = "¬" ++ paren f
+      paren f        = "(" ++ show f ++ ")"
+
+-- | A formula choice is associated with a boolean tag formula.
+--   If the formula is satisfied by the selection, the first alternative
+--   is selected; otherwise, the second alternative is selected.
+class ChcF r where
+  chcF :: Formula -> r a -> r a -> r a
+
+instance ChcF (Pretty c) where
+  chcF f a b = fromString (show f) <> "‹" <> a <> "," <> b <> "›"
+
+{-
 
 --
 -- * Decisions
@@ -75,17 +160,4 @@ psemS :: V String -> String
 psemS = unlines . map entry
   where entry (d,s) = concat (intersperse "," (map show d)) ++ "  ==>  " ++ s
 
-
---
--- * Variation annotations
---
-
--- | Tagless final representation of choices.
-class CC a where
-  -- | A choice introduces a point of variation in some dimension.
-  --   All choices in the same dimension are synchronized.
-  chc :: Dim -> a -> a -> a
-
--- | Variational evaluator
-instance CC (V a) where
-  chc d l r = qualify (Q d L) l ++ qualify (Q d R) r
+-}
