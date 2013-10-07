988fc291786c5ea6e0a0ec23bcaa8ce4f5115411
Eric Walkingshaw <eric@walkingshaw.net>
2013-10-07 11:26:52 +0200

    Refactor and add functions for parens.

--- src/Language/TTF/Pretty.hs
+++ src/Language/TTF/Pretty.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE
-      GeneralizedNewtypeDeriving
+      FlexibleInstances,
+      GeneralizedNewtypeDeriving,
+      TypeSynonymInstances
   #-}
 
 -- | A denotation used for stylized pretty printing.
 module Language.TTF.Pretty where
 
-import Data.Monoid (Monoid)
+import Data.Monoid (Monoid,(<>))
 import GHC.Exts (IsString(..))
 
 import System.Console.ANSI
@@ -13,18 +15,23 @@ import System.Console.ANSI
 
 -- * Denotations
 
+-- | A stylized string is a list of substrings with corresponding styles.
+type Stylized = [(Style,String)]
+
 -- | Semantics domain of pretty printed programs, represented by a function
---   from some context to a list of strings with corresponding styles.
-newtype Pretty c a = Pretty { inCtx :: c -> [(Style,String)] }
+--   from some context to a stylized string.
+newtype Pretty c a = Pretty { inCtx :: c -> Stylized }
   deriving Monoid
 
 -- | Stylize a plain string.
-style :: Style -> String -> Pretty c a
-style s x = Pretty $ \_ -> [(s,x)]
+style :: Style -> String -> Stylized
+style s x = [(s,x)]
 
 -- | String literals are interpreted as unstyled strings.
-instance IsString (Pretty c a) where
+instance IsString Stylized where
   fromString = style []
+instance IsString (Pretty c a) where
+  fromString = Pretty . const . style []
 
 -- | Pretty print with style to stdout.
 pretty :: c -> Pretty c a -> IO ()
@@ -33,6 +40,17 @@ pretty c p = sequence_ [setSGR s >> putStr x | (s,x) <- inCtx p c]
 -- | Get the pretty printed string without the style.
 asString :: c -> Pretty c a -> String
 asString c p = concatMap snd (inCtx p c)
+
+
+-- * Useful Functions
+
+-- | Add unstylized parentheses to a pretty printed value.
+parens :: Stylized -> Stylized
+parens = parens' []
+
+-- | Add stylized parentheses to a pretty printed value.
+parens' :: Style -> Stylized -> Stylized
+parens' s p = style s "(" <> p <> style s ")"
 
 
 -- * Styles
